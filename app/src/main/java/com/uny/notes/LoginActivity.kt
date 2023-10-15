cafd5fb2819b06908f10469a72617c87600f4500
Yoga Sulistiyo Widodo <chaintroopers88@gmail.com>
2023-10-15 20:01:42 +0700

    Config Firebase untuk login email dan password

--- app/src/main/java/com/uny/notes/LoginActivity.kt
+++ app/src/main/java/com/uny/notes/LoginActivity.kt
@@ -2,12 +2,20 @@ package com.uny.notes
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.uny.notes.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
@@ -15,16 +23,63 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mAuth = FirebaseAuth.getInstance()
 
 
-        binding.btnLogin.setOnClickListener{
-            val intent = Intent(this@LoginActivity, MainActivity::class.java)
-            startActivity(intent)
+
+        binding.btnLogin.setOnClickListener {
+            loginUserAccount()
+
         }
 
-        binding.tvRegister.setOnClickListener{
+        binding.tvRegister.setOnClickListener {
             val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun loginUserAccount() {
+        binding.progressbar.visibility = View.VISIBLE
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(
+                this,
+                "Please enter email!!",
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(
+                this,
+                "Please enter passwprd!!",
+                Toast.LENGTH_LONG
+            )
+                .show()
+        }
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+                override fun onComplete(p0: Task<AuthResult>) {
+                    if (p0.isSuccessful) {
+                        Toast.makeText(
+                            this@LoginActivity,
+                            "Login successful!!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        binding.progressbar.visibility = View.GONE
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(
+                            this@LoginActivity, "Login Failed!!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        binding.progressbar.visibility = View.GONE
+                    }
+                }
+
+            })
     }
 }
--- app/src/main/java/com/uny/notes/RegisterActivity.kt
+++ app/src/main/java/com/uny/notes/RegisterActivity.kt
@@ -1,12 +1,23 @@
 package com.uny.notes
 
+import android.app.ProgressDialog.show
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.android.play.integrity.internal.f
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.uny.notes.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(3000)
@@ -14,12 +25,58 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvLogin.setOnClickListener{
+        mAuth = FirebaseAuth.getInstance()
+
+        binding.tvLogin.setOnClickListener {
             onBackPressed()
+        }
+        binding.btnRegister.setOnClickListener {
+            registerNewUser()
         }
 
 
+    }
 
+    private fun registerNewUser() {
+        binding.progressbar.visibility = View.VISIBLE
+        val email = binding.etEmail.text.toString()
+        val password = binding.etPassword.text.toString()
 
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please Enter Your email!", Toast.LENGTH_SHORT)
+                .show()
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please Enter Your Password!", Toast.LENGTH_SHORT)
+                .show()
+        }
+        mAuth
+            .createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(object : OnCompleteListener<AuthResult> {
+                override fun onComplete(p0: Task<AuthResult>) {
+                    if (p0.isSuccessful) {
+                        Toast.makeText(
+                            getApplicationContext(),
+                            "Registration successful!",
+                            Toast.LENGTH_LONG
+                        )
+                            .show()
+                        binding.progressbar.visibility = View.GONE
+
+                        val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "Registration failed!! Please try again later",
+                            Toast.LENGTH_LONG
+                        )
+                            .show()
+                        binding.progressbar.visibility = View.GONE
+                    }
+
+                }
+
+            })
     }
 }
